b6164f35a28369b35934e2577536a12a5bd5ee31
Pytka Emil <emilpytka@gmail.com>
2012-06-15 15:23:05 +0200

    Upload project

---
+++ KinectRobotic/KinectRobotic/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using LegoNXTLibrary;
+using Microsoft.Kinect;
+
+namespace KinectRobotic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly KinectSensor kinectSensor;
+        byte[] pixelData;
+        Skeleton[] skeletons;
+        private LegoNXTDriver legoNxt;
+        private bool isConnectedToRobot = false;
+
+
+        private DateTime whenBeReady = DateTime.Now;
+        private bool IsRobotReady
+        {
+            get
+            {
+                if (whenBeReady < DateTime.Now)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public MainWindow()
+        {
+
+            kinectSensor = KinectSensor.KinectSensors[0];
+
+            Loaded += MainWindowLoaded;
+            Closing += MainWindowClosing;
+
+            kinectSensor.ColorStream.Enable();
+            kinectSensor.SkeletonStream.Enable();
+
+            InitializeComponent();
+        }
+
+        void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            kinectSensor.SkeletonFrameReady += KinectSensorSkeletonFrameReady;
+            kinectSensor.ColorFrameReady += KinectSensorColorFrameReady;
+            kinectSensor.Start();
+        }
+
+        void MainWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            kinectSensor.Stop();
+        }
+
+        void KinectSensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            bool receivedData = false;
+
+            using (ColorImageFrame cFrame = e.OpenColorImageFrame())
+            {
+                if (cFrame == null)
+                {
+                    // The image processing took too long. More than 2 frames behind.
+                }
+                else
+                {
+                    pixelData = new byte[cFrame.PixelDataLength];
+                    cFrame.CopyPixelDataTo(pixelData);
+                    receivedData = true;
+                }
+            }
+
+            if (receivedData)
+            {
+                BitmapSource source = BitmapSource.Create(640, 480, 96, 96,
+                        PixelFormats.Bgr32, null, pixelData, 640 * 4);
+                videoImage.Source = source;
+            }
+        }
+
+        void KinectSensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            bool receivedData = false;
+
+            using (SkeletonFrame sFrame = e.OpenSkeletonFrame())
+            {
+                if (sFrame == null)
+                {
+                    // The image processing took too long. More than 2 frames behind.
+                }
+                else
+                {
+                    skeletons = new Skeleton[sFrame.SkeletonArrayLength];
+                    sFrame.CopySkeletonDataTo(skeletons);
+                    receivedData = true;
+                }
+            }
+
+            if (receivedData)
+            {
+
+                Skeleton currentSkeleton = (from s in skeletons
+                                            where s.TrackingState == SkeletonTrackingState.Tracked
+                                            select s).FirstOrDefault();
+                if (currentSkeleton != null)
+                {
+                    WykrytyRuch(currentSkeleton);
+                }
+            }
+        }
+
+        private void WykrytyRuch(Skeleton currentSkeleton)
+        {
+            if (IsRobotReady)
+            {
+                if (currentSkeleton.Joints[JointType.Spine].Position.Z < 1.5)
+                {
+                    Warnings.Text = "Do przodu";
+                    if (isConnectedToRobot) legoNxt.MoveOneStepForward();
+ 
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(1);
+                }
+                else if (currentSkeleton.Joints[JointType.Spine].Position.Z > 2.5)
+                {
+                    Warnings.Text = "Do tyłu";
+                    if (isConnectedToRobot) legoNxt.MoveOneStepBackwards();
+                    
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(1);
+                }
+                else if (currentSkeleton.Joints[JointType.HandRight].Position.X < currentSkeleton.Joints[JointType.Spine].Position.X - 0.2)
+                {
+                    Warnings.Text = "Skręt w lewo";
+                    if (isConnectedToRobot) legoNxt.TurnLeft();
+                    
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(5);
+                }
+//                else if (currentSkeleton.Joints[JointType.HandRight].Position.X >
+//                         (currentSkeleton.Joints[JointType.Spine].Position.X + 0.4))
+                else if (currentSkeleton.Joints[JointType.HandLeft].Position.X > currentSkeleton.Joints[JointType.Spine].Position.X + 0.2)
+                {
+                    Warnings.Text = "Skręt w prawo";
+                    if (isConnectedToRobot) legoNxt.TurnRight();
+                    
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(5);
+
+                }
+                else if (currentSkeleton.Joints[JointType.HandRight].Position.Z + 0.6 <
+                         currentSkeleton.Joints[JointType.Spine].Position.Z)
+                {
+                    Warnings.Text = "Prawa reka do przodu";
+                    if (isConnectedToRobot) legoNxt.MoveArms(Direction.Left, 0); //drugi parametr nie używany :p
+
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(0.5);
+
+                }
+                else if (currentSkeleton.Joints[JointType.HandLeft].Position.Z + 0.6 <
+                         currentSkeleton.Joints[JointType.Spine].Position.Z)
+                {
+                    Warnings.Text = "Lewa reka do przodu";
+                    if (isConnectedToRobot) legoNxt.MoveArms(Direction.Right, 0); //drugi parametr nie używany :P 
+
+                    whenBeReady = DateTime.Now + TimeSpan.FromSeconds(0.5);
+                }
+                else
+                {
+                    Warnings.Text = "";
+                }
+            }
+        }
+
+        private void AboutClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(
+                "Program stworzony przez: Dawid Cichy, Marcin Dziarmaga, Damian Zawada, Jakub Izydorczyk, Emil Pytka",
+                "Grupa .NET FTIMS");
+        }
+
+        private void StopKinectClick(object sender, RoutedEventArgs e)
+        {
+            kinectSensor.Stop();
+        }
+
+        private void StartKinectClick(object sender, RoutedEventArgs e)
+        {
+            kinectSensor.Start();
+        }
+
+        private void ConnectToRobot(object sender, RoutedEventArgs e)
+        {
+            legoNxt = new LegoNXTDriver(0,"COM3");
+            isConnectedToRobot = true;
+        }
+
+        private void DisconnectRobot(object sender, RoutedEventArgs e)
+        {
+            isConnectedToRobot = false;
+            legoNxt.DisconnectNXT();
+        }
+
+        
+    }
+}
---
+++ LegoNXTLibrary/LegoNXTLibrary/LegoNXTDriver.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Robotics.Lego;
+
+namespace LegoNXTLibrary
+{
+    public class LegoNXTDriver
+    {
+        private int _armsAngle = 0;
+        private string _connectionPort;
+        private NXTBrick nxt = new NXTBrick();
+
+        public LegoNXTDriver(int armsAngle, string connectionPort)
+        {
+            _armsAngle = armsAngle;
+            _connectionPort = connectionPort;
+            ConnectToNXT();
+        }
+
+        private void ConnectToNXT()
+        {
+            nxt.Connect(_connectionPort);
+        }
+
+        //Moves the smallest step forwards
+        public void MoveOneStepForward()
+        {
+            LeftForward();
+            RightForward();
+        }
+
+        private void LeftForward()
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+            motorState.Power = 70;
+            motorState.TurnRatio = 50;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = 360;
+            nxt.SetMotorState(NXTBrick.Motor.C, motorState);
+        }
+
+        private void RightForward()
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+            motorState.Power = 70;
+            motorState.TurnRatio = 50;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = 360;
+            nxt.SetMotorState(NXTBrick.Motor.B, motorState);
+        }
+
+        //Moves the smallest step forwards
+        public void MoveOneStepBackwards()
+        {
+            RightBackwards();
+            LeftBackwards();
+        }
+
+        private void RightBackwards()
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+
+            motorState.Power = -70;
+            motorState.TurnRatio = -50;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = 360;
+
+            nxt.SetMotorState(NXTBrick.Motor.B, motorState);
+        }
+
+        private void LeftBackwards()
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+
+            motorState.Power = -70;
+            motorState.TurnRatio = -50;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = 360;
+
+            nxt.SetMotorState(NXTBrick.Motor.C, motorState);
+        }
+
+        private bool isFirstTurn = false;
+
+        //Turns one smallest step right
+        public void TurnRight()
+        {
+//            OneStepRight(180);
+//            if (isFirstTurn)
+//            {
+//                OneStepLeft(90);
+//                isFirstTurn = false;
+//            }
+            //tutaj skreca, po tej petli nalezy ustawic isFirstTurn = true;
+            for (int i = 0; i < 5; i++)
+                OneStepRight(360);
+            isFirstTurn = true;
+        }
+
+        //Turns one smallest step left
+        public void TurnLeft()
+        {
+//            OneStepLeft(180);
+//            if (isFirstTurn)
+//            {
+//                OneStepRight(90);
+//                isFirstTurn = false;
+//            }
+            //w tej petli skreca po niej nalezy ustawic isFirstTurn na true;
+            for (int i = 0; i < 5; i++)
+                OneStepLeft(360);
+            isFirstTurn = true;
+        }
+
+        private void OneStepLeft(int tacho)
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+
+            motorState.Power = 75;
+            motorState.TurnRatio = -100;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = tacho;
+
+            nxt.SetMotorState(NXTBrick.Motor.C, motorState);
+        }
+
+        private void OneStepRight(int tacho)
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+
+            motorState.Power = 75;
+            motorState.TurnRatio = 100;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = tacho;
+
+            nxt.SetMotorState(NXTBrick.Motor.B, motorState);
+        }
+
+        public void MoveArms(Direction direction, int angle)
+        {
+            if (_armsAngle < 90 && direction == Direction.Right)
+            {
+                MoveArm(80, 80, 10);
+                _armsAngle+=45;
+            }
+            else if (_armsAngle > -90 && direction == Direction.Left)
+            {
+                MoveArm(80, 80, -10);
+                _armsAngle-=45;
+            }
+        }
+
+        private void MoveArm(int angle, int power, int turnRatio)
+        {
+            NXTBrick.MotorState motorState = new NXTBrick.MotorState();
+
+            motorState.Power = power;
+            motorState.TurnRatio = turnRatio;
+            motorState.Mode = NXTBrick.MotorMode.On;
+            motorState.Regulation = NXTBrick.MotorRegulationMode.Speed;
+            motorState.RunState = NXTBrick.MotorRunState.Running;
+            motorState.TachoLimit = angle;
+
+            nxt.SetMotorState(NXTBrick.Motor.A, motorState);
+        }
+
+        public void DisconnectNXT()
+        {
+            nxt.Disconnect();
+        }
+    }
+}
